53bc0aacad39088dc0d8f7558af8686de93261cb
Ashish Shende <ashishpshende@gmail.com>
2024-02-23 03:02:58 +0530

    QR code Print

--- src/app/pages/private/qr-code/qr-code/qr-code.page.ts
+++ src/app/pages/private/qr-code/qr-code/qr-code.page.ts
@@ -16,15 +16,16 @@ import { LocalStorageService } from 'src/app/services/localStorage/local-storage
   styleUrls: ['./qr-code.page.scss'],
 })
 export class QrCodePage implements OnInit {
-  @ViewChild('printableContent', { static: false }) printableContent: ElementRef<any>;
+  @ViewChild('printableContent', { static: false })
+  printableContent: ElementRef<any>;
 
   public selectedParcel: Parcel;
   public loggedInUser: User;
-  public showPrint:boolean=false;
+  public showPrint: boolean = false;
   constructor(
     private parcelService: ParcelService,
     private router: Router,
-    private loaderService: LoaderService,    
+    private loaderService: LoaderService,
     private languageService: LanguageService,
     private localStorageService: LocalStorageService,
     public loadingController: LoadingController,
@@ -45,121 +46,230 @@ export class QrCodePage implements OnInit {
         }
       }
     });
-    if(this.authorizationService.loggedInUser.userRole!="SubDealer")
-    {
+    if (this.authorizationService.loggedInUser.userRole != 'SubDealer') {
       this.showPrint = true;
     }
   }
- 
-  print1(): void {
-   
-   // const elementId = this.elementRef.nativeElement.id;
-    const element = document.getElementById("printableContent");
-    //const printContents = this.printableContent.nativeElement.innerHTML;
-    const popupWindow = window.open('', '_blank', 'width=600,height=600');
-    popupWindow?.document.open();
-    popupWindow?.document.write(`
-      <html>
-        <head>
-          <style>
-          .printableContent {
-            margin: 10px;
-            border-color: black;
-            border-style: groove;
-            border-width: 1px;
-            padding: 30px;
-          }
-        
-          .qr-area {
-            text-align: center;
-            padding: 10px;
-          }
-          .parcel-number-area {
-            margin-top: 10px;
-            font-size: medium;
-          }
-          .bold-parcel-number {
-            font-size: large;
-            text-align: center;
-            margin-top: -30px;
-            color: black;
-          }
-          .parcel-attribute-area {
-            font-size: medium;
-            padding-left: 80px;
-          }
-          .parcel-attribute-row {
-            font-size: medium;
-            width: 100%;
-          }
-          .parcel-attribute-label {
-            font-weight: 100;
-            width: 50%;
-          }
-          .parcel-attribute-value {
-            width: 50%;
-            font-weight: 800;
-          }
-          .status-area {
-            font-size: medium;
-          }
-          </style>
-        </head>
-        <body onload="window.print(); window.onafterprint = window.close();">${element?.innerHTML}</body>
-      </html>
-    `);
-    popupWindow?.document.close();
-    // Handle canvas content
-    const canvasElements = popupWindow?.document.getElementsByTagName('canvas') ;
-    const tempCanvas = document.createElement('canvas');
-    const canvasArray =Array.prototype.slice.call(canvasElements);
 
-    canvasArray.forEach((canvas, index) => {
-      const ctx = tempCanvas.getContext('2d');
-      ctx?.drawImage(canvas, 0, 0);
-
-      // Replace the canvas with an image of its content
-      const img = document.createElement('img');
-      img.src = tempCanvas.toDataURL();
-      canvas.parentNode?.replaceChild(img, canvas);
-    });
-
-   
-
-    
-  }
-   print() {
-  const printWindow = window.open('', '_blank');
-  printWindow?.document.write('<html><head><title>Print</title></head><body>');
+  print() {
+    const printWindow = window.open('', '_blank', 'width=600,height=600');
+    printWindow?.document.open();
+    var starthtmlTags = `<html>
+    <head>
+      <title>CN NO: {{selectedParcel.cnNo}}</title>
+      <style>
+        .printableContent {
+          margin: 10px;
+          border-color: black;
+          border-style: groove;
+          border-width: 1px;
+          padding: 30px;
+          font-family: sans-serif;
+        }
   
-  const canvasElements = document.getElementsByTagName('canvas');
-  
-  for (let i = 0; i < canvasElements.length; i++) {
-    const canvas = canvasElements[i];
-    const img = new Image();
-    
-    // Convert canvas content to data URL
-    img.src = canvas.toDataURL('image/png');
-    
-    // Replace canvas with img in the print window
-    printWindow?.document.write('<div style="page-break-after:always;">');
-    printWindow?.document.write('<img src="' + img.src + '" style="width:100%;">');
-    printWindow?.document.write('</div>');
-  }
-  printWindow?.document.write('</body></html>');
-  printWindow?.document.close();
-  printWindow?.print();
-}
+        .qr-area {
+          text-align: center;
+          padding: 10px;
+        }
+        .qr-div {
+          margin-bottom: 20px;
+        }
+        .parcel-number-area {
+          margin-top: 10px;
+          font-size: medium;
+        }
+        .bold-parcel-number {
+          font-size: large;
+          text-align: center;
+          margin-top: -30px;
+          color: black;
+        }
+        .parcel-attribute-area {
+          font-size: medium;
+          padding-left: 80px;
+        }
+        .parcel-attribute-row {
+          font-size: medium;
+          width: 100%;
+        }
+        .parcel-attribute-label {
+          font-weight: 100;
+          width: 50%;
+        }
+        .parcel-attribute-value {
+          width: 50%;
+          font-weight: 800;
+        }
+        .status-area {
+          font-size: medium;
+        }
+      </style>
+    </head>
+    <body>`;   
+    var firstPart = `<div id="printableContent" class="printableContent">  <table style="width: 100%">    <tr>      <td>        <div class="qr-area">`;
+    var lastPart = `<div class="parcel-number-area">
+  <div class="bold-parcel-number">{{selectedParcel.cnNo}}</div>
+</div>
+</div>
+</td>
+<td>
+<div class="parcel-attribute-area">
+<div class="parcel-attribute-row">
+  <table style="width: 100%">
+    <tr>
+      <td class="parcel-attribute-label">
+        {{'PARCEL.CN_NO'}}:
+      </td>
+      <td class="parcel-attribute-value">
+        {{selectedParcel.cnNo}}
+      </td>
+    </tr>
+  </table>
+</div>
+<div class="parcel-attribute-row">
+  <table style="width: 100%">
+    <tr>
+      <td class="parcel-attribute-label">
+        {{'PARCEL.CN_TYPE'}}:
+      </td>
+      <td class="parcel-attribute-value">
+        {{selectedParcel.cnType}}
+      </td>
+    </tr>
+  </table>
+</div>
+<div class="parcel-attribute-row">
+  <table style="width: 100%">
+    <tr>
+      <td class="parcel-attribute-label">
+        {{'PARCEL.RECEIVER'}}:
+      </td>
+      <td class="parcel-attribute-value">
+        {{selectedParcel.receiver}}
+      </td>
+    </tr>
+  </table>
+</div>
+<div class="parcel-attribute-row">
+  <table style="width: 100%">
+    <tr>
+      <td class="parcel-attribute-label">
+        {{'PARCEL.MOBILE'}}:
+      </td>
+      <td class="parcel-attribute-value">
+        {{selectedParcel.mobile}}
+      </td>
+    </tr>
+  </table>
+</div>
+<div class="parcel-attribute-row">
+  <table style="width: 100%">
+    <tr>
+      <td  class="parcel-attribute-label">
+        {{'PARCEL.TO'}}:
+      </td>
+      <td class="parcel-attribute-value">
+        {{selectedParcel.to}}
+      </td>
+    </tr>
+  </table>
+</div>
+<div class="parcel-attribute-row">
+  <table style="width: 100%">
+    <tr>
+      <td class="parcel-attribute-label">
+        {{'PARCEL.QUANTITY'}}:
+      </td>
+      <td class="parcel-attribute-value">
+        {{selectedParcel.quantity}}
+      </td>
+    </tr>
+  </table>
+</div>
+<div class="parcel-attribute-row">
+  <table style="width: 100%">
+    <tr>
+      <td class="parcel-attribute-label">
+        {{'PARCEL.FROM'}}:
+      </td>
+      <td class="parcel-attribute-value">
+        {{selectedParcel.from}}
+      </td>
+    </tr>
+  </table>
+</div>
+</div>
+</td>
+</tr>
 
+</table>
+</div>
+</td>
+</tr>
+</table>
+    </div>`;
+    var endhtmlTags = `</body>
+    </html>`;
+    printWindow?.document.write(this.replaceLabelAndValues(starthtmlTags));
+    printWindow?.document.write(this.replaceLabelAndValues(firstPart));
+    const canvasElements = document.getElementsByTagName('canvas');
+
+    for (let i = 0; i < canvasElements.length; i++) {
+      const canvas = canvasElements[i];
+      const img = new Image();
+
+      // Convert canvas content to data URL
+      img.src = canvas.toDataURL('image/png');
+
+      // Replace canvas with img in the print window
+      printWindow?.document.write('<div class="qr-div">');
+      printWindow?.document.write(
+        '<img src="' + img.src + '" style="width:80%;">'
+      );
+      printWindow?.document.write('</div>');
+    }
+    printWindow?.document.write(this.replaceLabelAndValues(lastPart));
+    printWindow?.document.write(this.replaceLabelAndValues(endhtmlTags));
+    printWindow?.document.close();
+    printWindow?.print();
+  }
+  replaceLabelAndValues(text:string)
+  {
+    //Labels
+    text = text.replace("{{'PARCEL.CN_NO'}}", this.languageService.translate('PARCEL.CN_NO'));
+    text = text.replace("{{'PARCEL.CN_TYPE'}}", this.languageService.translate('PARCEL.CN_TYPE'));
+    text = text.replace("{{'PARCEL.RECEIVER'}}", this.languageService.translate('PARCEL.RECEIVER'));
+    text = text.replace("{{'PARCEL.MOBILE'}}", this.languageService.translate('PARCEL.MOBILE'));
+    text = text.replace("{{'PARCEL.TO'}}", this.languageService.translate('PARCEL.TO'));
+    text = text.replace("{{'PARCEL.QUANTITY'}}", this.languageService.translate('PARCEL.QUANTITY'));
+    text = text.replace("{{'PARCEL.FROM'}}", this.languageService.translate('PARCEL.FROM'));
+    text = text.replace("{{'PARCEL.STATUS'}}", this.languageService.translate('PARCEL.STATUS'));
+
+
+  
+    //values
+    text = text.replace('{{selectedParcel.cnNo}}',this.selectedParcel.cnNo);
+    text = text.replace('{{selectedParcel.cnNo}}',this.selectedParcel.cnNo);
+    text = text.replace('{{selectedParcel.cnNo}}',this.selectedParcel.cnNo);
+    text = text.replace('{{selectedParcel.cnType}}',this.selectedParcel.cnType);
+    text = text.replace('{{selectedParcel.receiver}}',this.selectedParcel.receiver);
+    text = text.replace('{{selectedParcel.mobile}}',this.selectedParcel.mobile??"");
+    text = text.replace('{{selectedParcel.to}}',this.selectedParcel.to??"");
+    text = text.replace('{{selectedParcel.quantity}}',this.selectedParcel.quantity);
+    text = text.replace('{{selectedParcel.from}}',this.selectedParcel.from);
+    text = text.replace('{{selectedParcel.parcelStatus}}',this.selectedParcel.parcelStatus);
+
+
+    return text;
+  }
   markAsDelivered() {
-
-    this.loaderService.customLoader("Updating Details...", 5000);
+    this.loaderService.customLoader('Updating Details...', 5000);
     this.selectedParcel.parcelStatus = KeywordConstants.PARCEL_STATUS_DELIVERED;
-    this.parcelService.updateStatus(this.selectedParcel,
+    this.parcelService.updateStatus(
+      this.selectedParcel,
       async (results: any) => {
         this.loaderService.dismissLoader();
         if (results.statusCode == 'SUCCESS') {
-
         } else {
         }
       },
@@ -193,7 +303,7 @@ export class QrCodePage implements OnInit {
     this.router.navigate(['home/parcel-list'], navigationExtras);
   }
   getParceDetails(cnNo: string) {
-    this.loaderService.customLoader("Loading Details...", 5000);
+    this.loaderService.customLoader('Loading Details...', 5000);
     this.parcelService.readByCnNo(
       cnNo,
       async (results: any) => {
@@ -207,26 +317,31 @@ export class QrCodePage implements OnInit {
       () => {}
     );
   }
-    //Alerts
-    async presentAlert(headerTitle = this.languageService.translate('LOGIN.ERROR_ALERT_TITLE'), message = this.languageService.translate('LOGIN.ERROR_INVALID_CREDENTIALS_MESSAGE')) {
-      const alert = await this.alertController.create({
-        cssClass: 'my-custom-class',
-        header: headerTitle,
-        subHeader: '',
-        message,
-        buttons: [
-          {
-            text: this.languageService.translate('BUTTONS.OK'),
-            role: 'confirm',
-            handler: () => {
-              //this.logout();
-            }
-          }
-        ]
-      });
-  
-      await alert.present();
-  
-      const { role } = await alert.onDidDismiss();
-    }
+  //Alerts
+  async presentAlert(
+    headerTitle = this.languageService.translate('LOGIN.ERROR_ALERT_TITLE'),
+    message = this.languageService.translate(
+      'LOGIN.ERROR_INVALID_CREDENTIALS_MESSAGE'
+    )
+  ) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: headerTitle,
+      subHeader: '',
+      message,
+      buttons: [
+        {
+          text: this.languageService.translate('BUTTONS.OK'),
+          role: 'confirm',
+          handler: () => {
+            //this.logout();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+  }
 }
